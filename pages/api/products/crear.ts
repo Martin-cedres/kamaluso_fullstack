3ee8689227ff1e42d846ec6bb58074c6d39a4d11
Martin-cedres <martinfernandocedres@gmail.com>
2025-09-10 14:22:14 -0300

    reverti cambios de normalizacion

--- pages/api/products/crear.ts
+++ pages/api/products/crear.ts
@@ -75,7 +75,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         precio: parseFloat(String(fields.precio || "0")) || 0,
         precioFlex: parseFloat(String(fields.precioFlex || "0")) || 0, // Add this line
         precioDura: parseFloat(String(fields.precioDura || "0")) || 0, // Add this line
-        categoria: String(fields.categoria || "sublimable").trim().toLowerCase().replace(/\s+/g, "-"),
+        categoria: String(fields.categoria || "sublimable"),
         subCategoria: subCategoriaField ? [subCategoriaField] : [], // Simplificado
         tapa: String(fields.tapa || ""),
         seoTitle: String(fields.seoTitle || ""),
